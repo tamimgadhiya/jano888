7b796d7c3e0fbbe1f086f8a2f86b2bba1425a93c
tamimgadhiya <158049478+tamimgadhiya@users.noreply.github.com>
2024-01-29 17:40:24 +0530

    jano888
    fisrt repositry

---
+++ assets/js/jano.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+  $('.accordian-p-1').hide();
+  $('.accordian-buttan-1').click(function(){
+    $('.accordian-p-1').slideToggle("slow");
+
+  });
+  $('.accordian-p-2').hide();
+  $('.accordian-buttan-2').click(function(){
+    $('.accordian-p-2').slideToggle("slow");
+
+  });
+  $('.accordian-p-3').hide();
+  $('.accordian-buttan-3').click(function(){
+    $('.accordian-p-3').slideToggle("slow");
+
+  });
+  $('.accordian-p-4').hide();
+  $('.accordian-buttan-4').click(function(){
+    $('.accordian-p-4').slideToggle("slow");
+
+  });
+  $('.accordian-p-5').hide();
+  $('.accordian-buttan-5').click(function(){
+    $('.accordian-p-5').slideToggle("slow");
+
+  });
+});
